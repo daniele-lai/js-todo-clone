e486e82919371cdd5bd0cee4a5dd8d04010c73ff
Daniele <danielelai86@gmail.com>
2021-03-18 17:15:28 +0100

    Aggiunti commenti

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
-
+//Evento keypress legato all'input
 $("#acquisti").keypress(function(event) {
   if (event.which == 13) {
     var ingrediente = $(this).val();
+
+    //Clono il list item del template in jQuery
     var listaClone = $(".template li").clone();
+
+    //Inseriamo il valore dell'input nell'elemento clonato
     listaClone.prepend(ingrediente);
+
+    //Mostriamo l'elemento all'interno del documento
     $(".lista").append(listaClone);
   }
 })
